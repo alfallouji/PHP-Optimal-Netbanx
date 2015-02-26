b1af1c42c919357a9cc8f39a268ac90bdf84adb7
Bashar <bashar.al-fallouji@ssense.com>
2015-02-27 09:38:14 +1100

    Updated config comment

--- tests/conf/credentials-sample.php
+++ tests/conf/credentials-sample.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Test config file
+ *
+ * You can find information needed from the developer center
+ * at https://developer.optimalpayments.com/en/my-account/
  */
 return array(
     'accountId' => '',
