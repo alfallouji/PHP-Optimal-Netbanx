f82312a79acc88a2f406f241909852992ee53d4e
Bashar <bashar.al-fallouji@ssense.com>
2015-02-27 23:43:07 +1100

    Added new models

---
+++ Optimal/Netbanx/Model/Error.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Note : Code is released under the GNU LGPL
+ *
+ * Please do not change the header of this file
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ */
+namespace Optimal\Netbanx\Model;
+
+/**
+ * File:        Error.php
+ * Project:     PHP-Optimal-Netbanx
+ *
+ * @author      Al-Fallouji Bashar
+ * 
+ * Error model
+ */
+class Error extends Base
+{
+    /**
+     * Bag properties definition 
+     * @var array
+     */
+    protected $_fields = array(
+        'code' => 'string',
+        'message' => 'string',
+        'details' => 'array',
+        'fielErrors' => 'array',
+    );
+}
